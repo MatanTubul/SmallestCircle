a99c87773dd2d8c455aff231a51067ac86428092
matant <matant86@gmail.com>
2015-04-20 11:37:03 +0300

    updating Cicle & Point classes

--- SmallestCircle/src/Circle.java
+++ SmallestCircle/src/Circle.java
@@ -1,4 +1,22 @@
 
 public class Circle {
+	
+	private Point center;
+	private double radius;
+	public Circle(Point p,double r)
+	{
+		this.center = new Point(p.getX(),p.getY());
+		this.radius = r;
+		
+	}
+	
+	public Point getCenter()
+	{
+		return this.center;
+	}
+	public double getRadius()
+	{
+		return this.radius;
+	}
 
 }
--- SmallestCircle/src/Point.java
+++ SmallestCircle/src/Point.java
@@ -1,4 +1,28 @@
 
 public class Point {
+	
+	private double x;
+	private double y;
+	
+	public Point(double n1,double n2)
+	{
+		this.x = n1;
+		this.y = n2;
+	}
+	
+	public double getX()
+	{
+		return this.x;
+	}
+	
+	public double getY()
+	{
+		return this.y;
+	}
+	
+	public double getDist(Point p)
+	{
+		return Math.sqrt((this.x - p.x)*(this.x - p.x) + (this.y - p.y)*(this.y - p.y));
+	}
 
 }
